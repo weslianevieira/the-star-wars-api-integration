eec4b9c2f74667d805e51b7d9056e34a599425aa
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-10-11 11:26:25 -0300

    application setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <Box>
+        
+      </Box>
+    )
+  }
+}
+
+export default App
